ceacbedb024097fcc61ab2876303ab6f5a5be071
fmarchi96 <federicomarchi@icloud.com>
2017-09-05 09:59:09 +0200

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
@@ -78,5 +78,3 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-    
-  
